cea2000d7306745b319a43a5af19f18db0833fd4
DeanParrish <parrish.de@gmail.com>
2014-05-25 16:46:23 -0400

    Added jQuery for active page

--- Site-Base/Scripts/main.js
+++ Site-Base/Scripts/main.js
@@ -3,10 +3,22 @@
 $(document).ready(function () {
     //clone jQuery nav ul to #menu-button && remove slash
     $("nav ul").clone().appendTo("#menu-button");
-    $("#menu-button ul li").remove(".slash");
+    $('nav ul li a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('navactive');
+//    $("nav ul li a").click(function () {
+//        alert("hit");
+//        $("nav ul li a").each(function () {
+//            $(this).removeClass("navactive");
+//        });
+
+//        $(this).addClass("navactive");
+//    });
+
+
 
     //slide function
     $('#menu-button').hover(function () {
         $(this).find('ul').stop().slideToggle(200);
     })
+
+
 });
